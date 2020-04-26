fab0d8e99c91b2e92ae1b59af11ddc6ab3a35593
Guebbit <guerzoni.andrea91@gmail.com>
2020-04-26 14:11:30 +0200

    Update Google.php

--- Google/Google.php
+++ Google/Google.php
@@ -1,13 +1,26 @@
-<?php
-
-namespace Guebbit\Google;
-
-use Guebbit\Base;
-
-class Google extends Base{
-	protected $google_url_lenght_limit=2048;	//deciso da google
-	protected $google_key="AIzaSyACyqv2JOOuwjt7Ob5COia6oE1vb2FDQUs";
-	// ----------- costants -----------
-	//self::CONSTANT_NAME
-	public const ERROR_URL_LIMIT_EXCEEDED = "Superato il limite di caratteri per Google Minimap";
-}
+<?php
+
+
+
+namespace Guebbit\Google;
+
+
+
+use Guebbit\Base;
+
+
+
+class Google extends Base{
+
+	protected $google_url_lenght_limit=2048;	//deciso da google
+
+	protected $google_key="XXXXXXXXXXXXX";		//non hardcodare
+
+	// ----------- costants -----------
+
+	//self::CONSTANT_NAME
+
+	public const ERROR_URL_LIMIT_EXCEEDED = "Superato il limite di caratteri per Google Minimap";
+
+}
+
